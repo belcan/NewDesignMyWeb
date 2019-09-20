ea551546da2b0c395f3db72ec03ad4b5f87f64b9
Nick <nikkurz@gmail.com>
2019-09-20 01:28:33 -0600

    added real equation

--- PBW/myscript.js
+++ PBW/myscript.js
@@ -2,21 +2,19 @@ function calculateM() {
     let a = document.getElementById("number").value;
     let b = parseInt(a);
 
-    if (a < 0 || a > 260) {
+    if (a < 0 || a > 230) {
         document.getElementById("calcpbm").innerHTML = "Ha-ha, very funny";
         document.getElementById("weight").innerHTML = "<img src= 'funnymusk.jpg'>";
-    } else if (a >= 50) {
+    } else if (a >= 152) {
+        let c = 50 + 0.905 * (b - 152.4)
         document.getElementById("calcpbm").innerHTML = "PBW for Male Patient";
-        document.getElementById("weight").innerHTML = b + 10 + ' kg';
+        document.getElementById("weight").innerHTML = c.toFixed(0) + ' kg';
     } else if (a < 1 && a >= 0) {
         document.getElementById("calcpbm").innerHTML = "Please enter hight in cm";
         document.getElementById("weight").innerHTML = "";
-    } else if (a >= 1 && a < 50) {
+    } else if (a >= 1 && a < 152) {
         document.getElementById("calcpbm").innerHTML = "C'mon, enter real hight!";
         document.getElementById("weight").innerHTML = "";
-    } else if (a < 0) {
-        document.getElementById("calcpbm").innerHTML = "Ha-ha, very funny";
-        document.getElementById("weight").innerHTML = "<img src= 'funnymusk.jpg'>";
     }
 }
 
@@ -24,16 +22,17 @@ function calculateF() {
     let a = document.getElementById("number").value;
     let b = parseInt(a);
 
-    if (a < 0 || a > 260) {
+    if (a < 0 || a > 230) {
         document.getElementById("calcpbm").innerHTML = "Ha-ha, very funny";
         document.getElementById("weight").innerHTML = "<img src= 'funnymusk.jpg'>";
-    } else if (a >= 50) {
+    } else if (a >= 152) {
+        let d = 45.5 + 0.905 * (b - 152.4)
         document.getElementById("calcpbm").innerHTML = "PBW for Female Patient";
-        document.getElementById("weight").innerHTML = b + 10 + ' kg';
+        document.getElementById("weight").innerHTML = d.toFixed(0) + ' kg';
     } else if (a < 1 && a >= 0) {
         document.getElementById("calcpbm").innerHTML = "Please enter hight in cm";
         document.getElementById("weight").innerHTML = "";
-    } else if (a >= 1 && a < 50) {
+    } else if (a >= 1 && a < 152) {
         document.getElementById("calcpbm").innerHTML = "C'mon, enter real hight!";
         document.getElementById("weight").innerHTML = "";
     }
