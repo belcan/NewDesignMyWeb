35dd5bea4ae648257b29523aef364a8fe171976a
nick <nick@nicks-MacBook-Air.local>
2021-02-20 18:02:46 -0700

    added alert for empty pt height

--- ARDSnetWeb/ARDSnet.js
+++ ARDSnetWeb/ARDSnet.js
@@ -1,58 +1,62 @@
-printButton.style.visibility='hidden';
+printButton.style.visibility = 'hidden';
 
 
-function calculateM() {    
-    
-   let a = document.getElementById("inputHeight").value;
-   let b = parseInt(a);    
-   let c = 50 + 0.905 * (b - 152.4);
-   
-        document.querySelector("#fourMil").innerHTML = '  ' + (c * 4).toFixed(0) + '  ml';
-        document.getElementById("fiveMil").innerHTML = '  ' +  (c * 5).toFixed(0) + ' ml';
-        document.getElementById("sixMil").innerHTML = '  ' +  (c * 6).toFixed(0) + ' ml';
-        document.getElementById("sevenMil").innerHTML = '   ' +  (c * 7).toFixed(0) + ' ml';
-        document.getElementById("eightMil").innerHTML = '   ' +  (c * 8).toFixed(0) + ' ml'; 
-        
-        calcButton.style.visibility='hidden';
-        printButton.style.visibility='visible';
+function calculateM() {
 
-        gsap.from('.volumes, output', {opacity: 0, duration: 1.1, ease: 'elastic.out(1.5, 0.3)', stagger: 0.2})
-    }
+  let a = document.getElementById("inputHeight").value;
+  let b = parseInt(a);
+  let c = 50 + 0.905 * (b - 152.4);
+  if (a === '') {
+    alert("Please Enter Paitent's Height")
+  } else {
+    document.querySelector("#fourMil").innerHTML = '  ' + (c * 4).toFixed(0) + '  ml';
+    document.getElementById("fiveMil").innerHTML = '  ' + (c * 5).toFixed(0) + ' ml';
+    document.getElementById("sixMil").innerHTML = '  ' + (c * 6).toFixed(0) + ' ml';
+    document.getElementById("sevenMil").innerHTML = '   ' + (c * 7).toFixed(0) + ' ml';
+    document.getElementById("eightMil").innerHTML = '   ' + (c * 8).toFixed(0) + ' ml';
 
+    calcButton.style.visibility = 'hidden';
+    printButton.style.visibility = 'visible';
+
+    gsap.from('.volumes, output', { opacity: 0, duration: 1.1, ease: 'elastic.out(1.5, 0.3)', stagger: 0.2 })
+  }
+}
 
 
 function calculateF() {
-    let d = document.getElementById("inputHeight").value;
-    let e = parseInt(d);    
-    let f = 45.5 + 0.905 * (e - 152.4);        
-        
-        document.getElementById("fourMil").innerHTML = (f * 4).toFixed(0) + ' ml';
-        document.getElementById("fiveMil").innerHTML = (f * 5).toFixed(0) + ' ml';
-        document.getElementById("sixMil").innerHTML = (f * 6).toFixed(0) + ' ml';
-        document.getElementById("sevenMil").innerHTML = (f * 7).toFixed(0) + ' ml';
-        document.getElementById("eightMil").innerHTML = (f * 8).toFixed(0) + ' ml';
+  let d = document.getElementById("inputHeight").value;
+  let e = parseInt(d);
+  let f = 45.5 + 0.905 * (e - 152.4);
+  if (d === '') {
+    alert("Please Enter Paitent's Height")
+  } else {
+    document.getElementById("fourMil").innerHTML = (f * 4).toFixed(0) + ' ml';
+    document.getElementById("fiveMil").innerHTML = (f * 5).toFixed(0) + ' ml';
+    document.getElementById("sixMil").innerHTML = (f * 6).toFixed(0) + ' ml';
+    document.getElementById("sevenMil").innerHTML = (f * 7).toFixed(0) + ' ml';
+    document.getElementById("eightMil").innerHTML = (f * 8).toFixed(0) + ' ml';
 
-         
-        calcButton.style.visibility='hidden';
-        printButton.style.visibility='visible';
 
-        gsap.from('.volumes, output', {opacity: 0, duration: 1.1, ease: 'elastic.out(1.5, 0.3)', stagger: 0.2})
-    }
-    
+    calcButton.style.visibility = 'hidden';
+    printButton.style.visibility = 'visible';
 
-    function doIt() {
-        var order = "m";
-        if (document.getElementById('radioMale').checked == true) {
-          order = calculateM();
-        } else if (document.getElementById('radioFemale').checked == true) {
-          order = calculateF();
-           
-      }
-    }
+    gsap.from('.volumes, output', { opacity: 0, duration: 1.1, ease: 'elastic.out(1.5, 0.3)', stagger: 0.2 })
+  }
+}
 
-gsap.from('.volumes p, output', {opacity: 0, duration: 1.1, ease: 'Power2.easeInOut', stagger: 0.1})
+function doIt() {
+  var order = "m";
+  if (document.getElementById('radioMale').checked == true) {
+    order = calculateM();
+  } else if (document.getElementById('radioFemale').checked == true) {
+    order = calculateF();
 
-gsap.from('#title', {opacity: 0, duration: 1.3, y: -40, ease: 'elastic.out(1.5, 0.3)'})
+  }
+}
+
+gsap.from('.volumes p, output', { opacity: 0, duration: 1.1, ease: 'Power2.easeInOut', stagger: 0.1 })
+
+gsap.from('#title', { opacity: 0, duration: 1.3, y: -40, ease: 'elastic.out(1.5, 0.3)' })
 
 //gsap.from('#fourMil', {opacity: 0, duration: 1.5, ease: 'Power2.easeInOut'})
 
